fc141ee92c0baeb2f996bc3376e8d8d432f63aa2
ivzx04 <ivan.af.paus@gmail.com>
2019-07-26 15:38:57 -0400

    Newly updated laserHist.

--- newHist.cpp
+++ newHist.cpp
@@ -22,7 +22,7 @@
 
 using namespace std;
 
-int stepSizeADC = 1000;
+int stepSizeADC = 5000;
 // given by Andrew on Friday April 12th 2019; modified after
 
 int main(int argc, char ** argv){
@@ -136,7 +136,7 @@ int main(int argc, char ** argv){
   graphADCList.push_back(graphA6);
  
   
-  double eventsADC = (double)(TreeH->GetEntries()/stepSizeADC);
+  double eventsADC = (double)(TreeH->GetEntries()/1000);
 
   cerr<<"Histograms and Trees successfully created\n";
   
@@ -144,7 +144,7 @@ int main(int argc, char ** argv){
 
   const double A = 0.025;
   ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-  for(int i = 0; i < TreeH->GetEntries(); i += stepSizeADC){
+  for(int i = 0; i < TreeH->GetEntries(); i += 1000){
     
      //Display completed
     if((i%10000) == 0){
@@ -162,14 +162,14 @@ int main(int argc, char ** argv){
     double T7 = dataT7[0] * A;
 
     double xTDC = (double)i * timeLength / (double)(TreeH->GetEntries());
-    graphT1 ->SetPoint(graphT1 ->GetN(),xTDC,dataT1[0]-dataT7[0]);
-    graphT2 ->SetPoint(graphT2 ->GetN(),xTDC,dataT2[0]-dataT7[0]);
-    graphT3 ->SetPoint(graphT3 ->GetN(),xTDC,dataT3[0]-dataT7[0]);
-    graphT4 ->SetPoint(graphT4 ->GetN(),xTDC,dataT4[0]-dataT7[0]);
-    graphTb1->SetPoint(graphTb1->GetN(),xTDC,sqrt(dataT1[0]*dataT2[0])-dataT7[0]);
-    graphTb2->SetPoint(graphTb2->GetN(),xTDC,sqrt(dataT3[0]*dataT4[0])-dataT7[0]);
-    graphT5 ->SetPoint(graphT5 ->GetN(),xTDC,dataT5[0]-dataT7[0]);
-    graphT6 ->SetPoint(graphT6 ->GetN(),xTDC,dataT6[0]-dataT7[0]);
+    graphT1 ->SetPoint(graphT1 ->GetN(),xTDC,T1-T7);
+    graphT2 ->SetPoint(graphT2 ->GetN(),xTDC,T2-T7);
+    graphT3 ->SetPoint(graphT3 ->GetN(),xTDC,T3-T7);
+    graphT4 ->SetPoint(graphT4 ->GetN(),xTDC,T4-T7);
+    graphTb1->SetPoint(graphTb1->GetN(),xTDC,sqrt(T1*T2)-T7);
+    graphTb2->SetPoint(graphTb2->GetN(),xTDC,sqrt(T3*T4)-T7);
+    graphT5 ->SetPoint(graphT5 ->GetN(),xTDC,T5-T7);
+    graphT6 ->SetPoint(graphT6 ->GetN(),xTDC,T6-T7);
   
     double xADC = (double)i * timeLength / (double)(TreeH->GetEntries());
     graphA1 ->SetPoint(graphA1 ->GetN(),xADC,dataA1[0]);
@@ -186,38 +186,129 @@ int main(int argc, char ** argv){
   ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
    TGraphAsymmErrors * grA1 = new TGraphAsymmErrors();
    TGraphAsymmErrors * grA2 = new TGraphAsymmErrors();
+   TGraphAsymmErrors * grA3 = new TGraphAsymmErrors();
+   TGraphAsymmErrors * grA4 = new TGraphAsymmErrors();
+   TGraphAsymmErrors * grA5 = new TGraphAsymmErrors();
+   TGraphAsymmErrors * grA6 = new TGraphAsymmErrors();
    
   grA1->SetName("ADC_Averages_Graph1");
   grA2->SetName("ADC_Averages_Graph2");
+  grA3->SetName("ADC_Averages_Graph3");
+  grA4->SetName("ADC_Averages_Graph4");
+  grA5->SetName("ADC_Averages_Graph5");
+  grA6->SetName("ADC_Averages_Graph6");
+
+   TGraphAsymmErrors * grT1 = new TGraphAsymmErrors();
+   TGraphAsymmErrors * grT2 = new TGraphAsymmErrors();
+   TGraphAsymmErrors * grT3 = new TGraphAsymmErrors();
+   TGraphAsymmErrors * grT4 = new TGraphAsymmErrors();
+   TGraphAsymmErrors * grT5 = new TGraphAsymmErrors();
+   TGraphAsymmErrors * grT6 = new TGraphAsymmErrors();
+   
+  grT1->SetName("TDC_Averages_Graph1");
+  grT2->SetName("TDC_Averages_Graph2");
+  grT3->SetName("TDC_Averages_Graph3");
+  grT4->SetName("TDC_Averages_Graph4");
+  grT5->SetName("TDC_Averages_Graph5");
+  grT6->SetName("TDC_Averages_Graph6");
+
+
   
  for(int i=0;i<TreeH->GetEntries();i=i+stepSizeADC){
+ double sumA = 0;
+ double sumT = 0;
+
+ double sumA2 = 0;
+ double sumT2 = 0;
+
+ double sumA3 = 0;
+ double sumT3 = 0;
+
+ double sumA4 = 0;
+ double sumT4 = 0;
+
+ double sumA5 = 0;
+ double sumT5 = 0;
+
+ double sumA6 = 0;
+ double sumT6 = 0;
+
  double xADC = (double)i * timeLength / (double)(TreeH->GetEntries());
- //cout <<i<<"\n";
- double sum = 0;
+ double xTDC = (double)i * timeLength / (double)(TreeH->GetEntries());
+
  
-    for(int j=i;j<(i+stepSizeADC);j++){
+
+ for(int j=i;j<(i+stepSizeADC);j++){
 
       TreeH->GetEntry(j);
-      sum = sum+dataA1[0];
-      
-    }
-    double mean= sum/stepSizeADC;
-    grA1->SetPoint(grA1->GetN(),xADC,mean);
-   }
- 
- for(int i=0;i<TreeH->GetEntries();i=i+stepSizeADC){
- double xADC = (double)i * timeLength / (double)(TreeH->GetEntries());
- double sum = 0;
- 
-    for(int j=i;j<(i+stepSizeADC);j++){
 
-      TreeH->GetEntry(j);
-      sum = sum+dataA2[0];
-      
+
+    double T1 = dataT1[0] * A;
+    double T2 = dataT2[0] * A;
+    double T3 = dataT3[0] * A;
+    double T4 = dataT4[0] * A;
+    double T5 = dataT5[0] * A;
+    double T6 = dataT6[0] * A;    
+    double T7 = dataT7[0] * A;
+ 
+      sumA = sumA+dataA1[0];
+      sumT = sumT+(T1-T7);
+
+      sumA2 = sumA2+dataA2[0];
+      sumT2 = sumT2+(T2-T7);
+
+      sumA3 = sumA3+dataA3[0];
+      sumT3 = sumT3+(T3-T7);
+
+      sumA4 = sumA4+dataA4[0];
+      sumT4 = sumT4+(T4-T7);
+
+      sumA5 = sumA5+dataA5[0];
+      sumT5 = sumT5+(T5-T7);
+
+      sumA6 = sumA6+dataA6[0];
+      sumT6 = sumT6+(T6-T7);
     }
-    double mean= sum/stepSizeADC;
-    grA2->SetPoint(grA2->GetN(),xADC,mean);
-   }  
+    
+    double meanA= sumA/stepSizeADC;
+    grA1->SetPoint(grA1->GetN(),xADC,meanA);
+
+    double meanT= sumT/stepSizeADC;
+    grT1->SetPoint(grT1->GetN(),xTDC,meanT); 
+
+    double meanA2= sumA2/stepSizeADC;
+    grA2->SetPoint(grA2->GetN(),xADC,meanA2);
+
+    double meanT2= sumT2/stepSizeADC;
+    grT2->SetPoint(grT2->GetN(),xTDC,meanT2);
+
+     double meanA3= sumA3/stepSizeADC;
+    grA3->SetPoint(grA3->GetN(),xADC,meanA3);
+
+    double meanT3= sumT3/stepSizeADC;
+    grT3->SetPoint(grT3->GetN(),xTDC,meanT3);
+
+    double meanA4= sumA4/stepSizeADC;
+    grA4->SetPoint(grA4->GetN(),xADC,meanA4);
+
+    double meanT4= sumT4/stepSizeADC;
+    grT4->SetPoint(grT4->GetN(),xTDC,meanT4);
+
+    double meanA5= sumA5/stepSizeADC;
+    grA5->SetPoint(grA5->GetN(),xADC,meanA5);
+
+    double meanT5= sumT5/stepSizeADC;
+    grT5->SetPoint(grT5->GetN(),xTDC,meanT5);
+
+    double meanA6= sumA6/stepSizeADC;
+    grA6->SetPoint(grA6->GetN(),xADC,meanA6);
+
+    double meanT6= sumT6/stepSizeADC;
+    grT6->SetPoint(grT6->GetN(),xTDC,meanT6);
+    
+ }
+ 
+
   
   
   inputFile->Close();
@@ -227,6 +318,18 @@ int main(int argc, char ** argv){
   outputFile->cd();
     grA1->Write();
     grA2->Write();
+    grA3->Write();
+    grA4->Write();
+    grA5->Write();
+    grA6->Write();
+
+    grT1->Write();
+    grT2->Write();
+    grT3->Write();
+    grT4->Write();
+    grT5->Write();
+    grT6->Write();
+    
   for (int i = 0; i < graphADCList.size(); i++){graphADCList[i]->Write();}
   for (int i = 0; i < graphTDCList.size(); i++){graphTDCList[i]->Write();}
   
