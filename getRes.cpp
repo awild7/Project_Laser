ac0fa5d2d5c32e641aa88aeaf110fcca629b61ad
Andrew Denniston <awild@mit.edu>
2019-08-06 17:33:51 -0400

    Made the vectors into arrays and streamlined the code

--- getRes.cpp
+++ getRes.cpp
@@ -16,16 +16,16 @@
 
 using namespace std;
 
-double minRes = -1;
-double maxRes = 1;
-int stepSize = 300000;
+int stepSize = 10000;
+int np = 7;
+int ng = 7;
+const double A = 0.025;
 
 void failReadIn(){
   cerr<<"the input text file should be constructed\n"
       <<"with three rows for the PMT number, the "
       <<"first parameter p1, and the second parameter"
       <<"p0. It should start at PMT 0 and go to 9\n\n"
-      <<"0 [p1 of PMT 0] [p0 of PMT 0]\n"
       <<"1 [p1 of PMT 1] [p0 of PMT 1]\n"
       <<".\n"
       <<".\n"
@@ -55,8 +55,8 @@ int main(int argc, char ** argv){
   double timeLength = atoi(argv[4]);
   int counter=0;
   int PMTnumber;
-  double c1[10];
-  double c0[10];
+  double c1[9];
+  double c0[9];
 
   if( !inParamFile.is_open() ){
     cerr<<"The text file could not be opened.\n Aborting...\n\n";
@@ -66,7 +66,7 @@ int main(int argc, char ** argv){
     if(counter != PMTnumber){
       failReadIn();
     }
-    inParamFile >> c1[PMTnumber] >> c0[PMTnumber];
+    inParamFile >> c1[counter] >> c0[counter];
     counter++;
   } 
    
@@ -77,6 +77,8 @@ int main(int argc, char ** argv){
 
   
   //Define variables needed for histograms
+  double dA[np];
+  double dataA0[99] = {0.};
   double dataA1[99] = {0.};
   double dataA2[99] = {0.};
   double dataA3[99] = {0.};
@@ -84,6 +86,8 @@ int main(int argc, char ** argv){
   double dataA5[99] = {0.};
   double dataA6[99] = {0.};
 
+  double dT[np];
+  double dataT0[99] = {0.};
   double dataT1[99] = {0.};
   double dataT2[99] = {0.};
   double dataT3[99] = {0.};
@@ -95,6 +99,7 @@ int main(int argc, char ** argv){
   
   
   //Set addresses for data (D_A00)
+  TreeH->SetBranchAddress("D.A00",&dataA0);
   TreeH->SetBranchAddress("D.A01",&dataA1);
   TreeH->SetBranchAddress("D.A02",&dataA2);
   TreeH->SetBranchAddress("D.A03",&dataA3);
@@ -102,6 +107,7 @@ int main(int argc, char ** argv){
   TreeH->SetBranchAddress("D.A05",&dataA5);
   TreeH->SetBranchAddress("D.A06",&dataA6);
 
+  TreeH->SetBranchAddress("D.T000",&dataT0);
   TreeH->SetBranchAddress("D.T001",&dataT1);
   TreeH->SetBranchAddress("D.T002",&dataT2);
   TreeH->SetBranchAddress("D.T003",&dataT3);
@@ -111,117 +117,142 @@ int main(int argc, char ** argv){
   TreeH->SetBranchAddress("D.T007",&dataT7);
 
   //Now make a vector of graphs for the resolution graphs
-  vector<TGraphAsymmErrors*> graphResList;
-  TGraphAsymmErrors * graphresT1 = new TGraphAsymmErrors();
-  graphresT1->SetName("ToF_res_1_time");
-  graphResList.push_back(graphresT1);
-  TGraphAsymmErrors * graphresT2 = new TGraphAsymmErrors();
-  graphresT2->SetName("ToF_res_2_time");
-  graphResList.push_back(graphresT2);
-  TGraphAsymmErrors * graphresT3 = new TGraphAsymmErrors();
-  graphresT3->SetName("ToF_res_3_time");
-  graphResList.push_back(graphresT3);
-  TGraphAsymmErrors * graphresT4 = new TGraphAsymmErrors();
-  graphresT4->SetName("ToF_res_4_time");
-  graphResList.push_back(graphresT4);
-  TGraphAsymmErrors * graphresT5 = new TGraphAsymmErrors();
-  graphresT5->SetName("ToF_res_5_time");
-  graphResList.push_back(graphresT5);
-  TGraphAsymmErrors * graphresT6 = new TGraphAsymmErrors();
-  graphresT6->SetName("ToF_res_6_time");
-  graphResList.push_back(graphresT6);
-
-  //Start by defining histograms to get the standard deviations
-  vector<TH1*> sliceHistList;
-  TH1D * slice_T1 = new TH1D("sliceT1","sliceT1",40,minRes,maxRes);
-  sliceHistList.push_back(slice_T1);
-  TH1D * slice_T2 = new TH1D("sliceT1","sliceT1",40,minRes,maxRes);
-  sliceHistList.push_back(slice_T2);
-  TH1D * slice_T3 = new TH1D("sliceT1","sliceT1",40,minRes,maxRes);
-  sliceHistList.push_back(slice_T3);
-  TH1D * slice_T4 = new TH1D("sliceT1","sliceT1",40,minRes,maxRes);
-  sliceHistList.push_back(slice_T4);
-  TH1D * slice_T5 = new TH1D("sliceT1","sliceT1",40,minRes,maxRes);
-  sliceHistList.push_back(slice_T5);
-  TH1D * slice_T6 = new TH1D("sliceT1","sliceT1",40,minRes,maxRes);
-  sliceHistList.push_back(slice_T6);
-
-  const double A = 0.025;
+  TGraphAsymmErrors * graphTimeList[ng];
+  TGraphAsymmErrors * graphResList[ng];
+  TH1D * coarseHistList[ng];
   
+  for(int k = 0; k < ng; k++){
+
+    char tempGT[100];
+    sprintf(tempGT,"ToF_%d_time",k);
+    graphTimeList[k] = new TGraphAsymmErrors();
+    graphTimeList[k]->SetName(tempGT);
+
+    char tempGR[100];
+    sprintf(tempGR,"ToF_Res_%d_time",k);
+    graphResList[k] = new TGraphAsymmErrors();
+    graphResList[k]->SetName(tempGR);
+
+    char tempC[100];
+    sprintf(tempC,"coarse_hist_%d",k);
+    coarseHistList[k] = new TH1D(tempC,tempC,400,-100,100);
+  }
+  
+
+  //Loop over every stepSize points
   for(int i = 0; i < (TreeH->GetEntries()); i += stepSize){
 
     //Display completed
-    if((i%(stepSize*10)) == 0){
-      cerr << (i*100.)/(TreeH->GetEntries()) <<"% complete\n";
-    }  
+    cerr << (i*100.)/(TreeH->GetEntries()) <<"% complete of averaged loops\n";
+
+        //First find the mean and average coarsly and then look closely
     for(int j = i; j < (i + stepSize); j++){         
       if(j >= TreeH->GetEntries()){
 	continue;
-      }
-      //Get the entries at j
-      TreeH->GetEntry(j);
-      //Convet TDC to units of picosecons
-      double T1 = dataT1[0] * A;
-      double T2 = dataT2[0] * A;
-      double T3 = dataT3[0] * A;
-      double T4 = dataT4[0] * A;
-      double T5 = dataT5[0] * A;
-      double T6 = dataT6[0] * A;    
-      double T7 = dataT7[0] * A;	 	 
-      //now calculate the resolution of time with the slice histograms and then fill the vectors    
+      }      
 
-      //Make Time of Flight corrections
-      T1 -= (c1[1]/sqrt(dataA1[0])) + c0[1];
-      T2 -= (c1[2]/sqrt(dataA2[0])) + c0[2];
-      T3 -= (c1[3]/sqrt(dataA3[0])) + c0[3];
-      T4 -= (c1[4]/sqrt(dataA4[0])) + c0[4];
-      T5 -= (c1[5]/sqrt(dataA5[0])) + c0[5];
-      T6 -= (c1[6]/sqrt(dataA6[0])) + c0[6];
+      TreeH->GetEntry(j); //Get the entries at j
+      //Define the variables in the arrays
+      dT[0] = dataT0[0];
+      dT[1] = dataT1[0];
+      dT[2] = dataT2[0];
+      dT[3] = dataT3[0];
+      dT[4] = dataT4[0];
+      dT[5] = dataT5[0];
+      dT[6] = dataT6[0];
+
+      dA[0] = dataA0[0];
+      dA[1] = dataA1[0];
+      dA[2] = dataA2[0];
+      dA[3] = dataA3[0];
+      dA[4] = dataA4[0];
+      dA[5] = dataA5[0];
+      dA[6] = dataA6[0];
       
-      slice_T3->Fill(T3-T7);
-      slice_T4->Fill(T4-T7);
-      slice_T1->Fill(T1-T7);
-      slice_T2->Fill(T2-T7);
-      slice_T5->Fill(T5-T7);
-      slice_T6->Fill(T6-T7);
+      double Tref = dataT7[0] * A;	 	 
+      for(int k = 0; k < np; k++){
+	double time = dT[k] * A;              //Convert to picoseconds
+	time -= (c1[k]/sqrt(dA[k])) + c0[k];  //Time walk correction
+	time -= Tref;                           //Reference time
+	coarseHistList[k]->Fill(time);         //Fill histogram
       }
+    }
 
-    double xSlice = (double)i * timeLength / ((double)(TreeH->GetEntries()));
-
+    //Define narrow histograms
+    TH1D * fineHistList[ng];
+    double minFine[ng];
+    double maxFine[ng];
+    for(int k = 0; k < ng; k++){
+      char tempF[100];
+      sprintf(tempF,"fine_hist_%d",k);
+      minFine[k] = (coarseHistList[k]->GetMean()) - (4 * (coarseHistList[k]->GetStdDev()));
+      maxFine[k] = (coarseHistList[k]->GetMean()) + (4 * (coarseHistList[k]->GetStdDev()));
+      fineHistList[k] = new TH1D(tempF,tempF,400,minFine[k],maxFine[k]);
+    }
     
-    //Now I can fit my slices to my gausians
-    for (int k = 0; k < sliceHistList.size(); k++){
-      TF1 * myfit = new TF1("myFit","gaus",minRes,maxRes);
-      myfit->SetParameter(0,sliceHistList[k]->GetMaximum());
-      myfit->SetParameter(1,sliceHistList[k]->GetMean());
-      myfit->SetParameter(2,sliceHistList[k]->GetStdDev());
-
-      if(k=0){
-	sliceHistList[k]->Write();
-      }
+    //Now run the loop again while looking more narrowly
+    for(int j = i; j < (i + stepSize); j++){         
+      if(j >= TreeH->GetEntries()){
+	continue;
+      }      
       
-      if( (sliceHistList[k]->GetEntries()) > 1000 ){
-	TFitResultPtr point = sliceHistList[k]->Fit(myfit,"qeSrn","",minRes,maxRes);
-	int fitStatus = point;
-	double res;
-	if(fitStatus == 0){
-	  res = point->Parameter(2);
-	}
-	else{
-	  res = sliceHistList[k]->GetMean();	
-	}
-	graphResList[k]->SetPoint(graphResList[k]->GetN(),xSlice,res);
+      TreeH->GetEntry(j); //Get the entries at j
+      //Define the variables in the arrays
+      dT[0] = dataT0[0];
+      dT[1] = dataT1[0];
+      dT[2] = dataT2[0];
+      dT[3] = dataT3[0];
+      dT[4] = dataT4[0];
+      dT[5] = dataT5[0];
+      dT[6] = dataT6[0];
+
+      dA[0] = dataA0[0];
+      dA[1] = dataA1[0];
+      dA[2] = dataA2[0];
+      dA[3] = dataA3[0];
+      dA[4] = dataA4[0];
+      dA[5] = dataA5[0];
+      dA[6] = dataA6[0];
+      
+      double Tref = dataT7[0] * A;	 	 
+
+      for(int k = 0; k < ng; k++){
+	double time = dT[k] * A;              //Convert to picoseconds
+	time -= (c1[k]/sqrt(dA[k])) + c0[k];  //Time walk correction
+	time -= Tref;                           //Reference time
+	fineHistList[k]->Fill(time);         //Fill histogram
       }
+    }
 
-      /*
+    //Now we can make the fit
+    double xCoarse = (double)i * timeLength / ((double)(TreeH->GetEntries()));
+
+    //Now I can fit my coarses to my gausians
+    for(int k = 0; k < ng; k++){
+      TF1 * myfit = new TF1("myFit","gaus",minFine[k],maxFine[k]);   //Define Fit
+      myfit->SetParameter(0,fineHistList[k]->GetMaximum());  //Find start parameters for a more acurate fit
+      myfit->SetParameter(1,fineHistList[k]->GetMean());
+      myfit->SetParameter(2,fineHistList[k]->GetStdDev());
+      TFitResultPtr point = coarseHistList[k]->Fit(myfit,"qeSrn","",minFine[k],maxFine[k]); //Fit in the histogram range and use point to store the result
       int fitStatus = point;
-      double res = -1000;      	
-      if(fitStatus == 0){
-	cout<<"lsakjfsalkfj\n";
 
-	}*/
-     
-      sliceHistList[k]->Reset();
+      //Fill with resolution
+      double time;
+      double resolution;
+      if(fitStatus == 0){
+	time = point->Parameter(1);
+	resolution = point->Parameter(2);
+      }
+      else{
+	time = -1000;//fineHistList[k]->GetMean();
+	resolution = -100;// fineHistList[k]->GetStdDev();
+      }
+      graphTimeList[k]->SetPoint(graphTimeList[k]->GetN(),xCoarse,time);
+      graphResList[k]->SetPoint(graphResList[k]->GetN(),xCoarse,resolution);
+
+      //Reset the coarse histogram
+      delete fineHistList[k];
+      coarseHistList[k]->Reset();
     }
   }
     
@@ -232,8 +263,12 @@ int main(int argc, char ** argv){
 
   //Write out
   outputFile->cd();  
-  for (int i = 0; i < graphResList.size(); i++){graphResList[i]->Write();}
+  for (int k = 0; k < ng; k++){
+    graphTimeList[k]->Write();
+    graphResList[k]->Write();
+  }
   outputFile->Close();
+  
   cerr<< argv[2]<<" has been completed. \n\n\n";
   
   return 0;
